b494961c9cfae9efb3995e20b3e4a26003969d7b
Omar Aref <36766407+OmarKnows@users.noreply.github.com>
2022-05-15 10:55:37 +0200

    Added header, footer, homescreen and product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './components/Screens/HomeScreen'
+import { Container } from 'react-bootstrap'
+
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Welcome To ProShop</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen></HomeScreen>
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
