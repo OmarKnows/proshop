58e462d6c6b47469228f2ac67678aa99a47b3817
Omar Aref <36766407+OmarKnows@users.noreply.github.com>
2022-05-18 10:00:59 +0200

    added product details page and finished basic frontend functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './components/Screens/HomeScreen'
-import ProductScreen from './components/Screens/ProductScreen'
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
 import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
@@ -13,7 +13,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
-            <Route path='/product/:id' component={ProductScreen} />
+            <Route path='product/:id' element={<ProductScreen />} />
           </Routes>
         </Container>
       </main>
---
+++ frontend/src/Screens/ProductScreen.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
+
+const ProductScreen = () => {
+  let params = useParams()
+  const product = products.find((p) => p._id === params.id)
+  return (
+    <div>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col>
+          <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col>
+          <ListGroup variant='flush'>
+            <ListGroupItem>
+              <h3>{product.name}</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} Reviews`}
+              />
+            </ListGroupItem>
+            <ListGroupItem>Price: ${product.price}</ListGroupItem>
+            <ListGroupItem>Description: {product.description}</ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroupItem>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock ? 'In Stock' : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default ProductScreen
