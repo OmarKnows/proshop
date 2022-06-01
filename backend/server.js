a54a103d12faa645a42435c01bc14d8ea7281854
OmarKnows <omarknows98@gmail.com>
2022-06-01 14:19:18 +0200

    add product routes from db && custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import colors from 'colors'
 
 dotenv.config()
 
@@ -15,20 +16,16 @@ app.use(cors())
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
+app.use('/api/products', productRoutes)
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id == req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
-const MODE = process.env.NODE_ENV
 
 app.listen(
   PORT,
-  console.log(`server running in ${MODE} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
